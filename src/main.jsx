83228934250f6e3097999fab4107659234397c9d
Tomane Mateus Tomane <padacius@gmail.com>
2026-07-10 16:38:55 -0300

    Use HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>
 )
